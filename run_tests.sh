e449c0ef5fd0b01f42a24de1b271d62ada89bf36
Oleg Lomaka <oleg.lomaka@gmail.com>
2025-02-13 09:25:25 -0500

    setup tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,15 +5,5 @@ set -x
 
 mkdir -p temp
 
-package/bin/authV2 \
-  testdata/authV2_input.json \
-  temp/authV2_witness.wtns
-snarkjs groth16 prove \
-  circuits/authV2/circuit_final.zkey \
-  temp/authV2_witness.wtns \
-  temp/authV2_proof.json \
-  temp/authV2_public.json
-snarkjs groth16 verify \
-  circuits/authV2/verification_key.json \
-  temp/authV2_public.json \
-  temp/authV2_proof.json
+package/bin/authV2 testdata/authV2_input.json temp/authV2_witness.wtns
+snarkjs wchk testdata/authV2.r1cs temp/authV2_witness.wtns
